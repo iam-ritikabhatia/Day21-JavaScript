df5f31f7e758c10b22f27d89fc6a8237aafa919d
Ritika Bhatia <139344947+iam-ritikabhatia@users.noreply.github.com>
2024-01-21 21:21:59 +0530

    Add files via upload

---
+++ Day21.js
@@ -0,0 +1,32 @@
+// This Property represents Euler's Number, e = 2.178.....
+
+let a = Math.E;
+let b = a.toFixed(7);
+console.log(b); // Output is 2.7182818
+
+// This Property represents Euler's Number, pi = 3.14.....
+
+let a = Math.PI;
+let b = a.toFixed(7);
+console.log(b); // Output 3.1415927
+
+// This Property represents natural log of 10
+
+let a = Math.LN10;
+console.log(a);
+
+// This Property represents natural log of 2
+
+console.log(Math.LN2);
+
+// sqrt
+console.log(Math.SQRT1_2);
+
+// Math.abs.js
+
+// This math function is used to find the absolute(non negative) value of the difference of two numbers
+
+let a = 2;
+let b = 3;
+let c = Math.abs(a - b);
+console.log(c);
